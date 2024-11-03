e4a3e8dc9e19dc059d0ebff8068be1c5c53814a0
Geoff Surmanski <gsurmans@gmail.com>
2024-11-02 21:20:01 -0400

    first commit

---
+++ commerce/auctions/models.py
@@ -0,0 +1,41 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+from django.core.validators import MinValueValidator
+
+
+class User(AbstractUser):
+    pass
+
+class Listing(models.Model):
+    title = models.CharField(max_length=50)
+    start_bid = models.DecimalField(
+        max_digits=10,
+        decimal_places=2,
+        validators=[MinValueValidator(0.01)], 
+        default=0.01
+        )
+    
+    current_price = models.DecimalField(
+        max_digits=10,
+        decimal_places=2
+        )
+    
+    description = models.TextField(max_length=500, default="")
+    image_url = models.TextField(max_length=500, default="")
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
+
+class Bid(models.Model):
+    amount = models.IntegerField(validators=[MinValueValidator(0.01)])
+
+    listing = models.ForeignKey(Listing, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+class Comment(models.Model):
+    text = models.TextField(max_length=500, default="")
+
+    listing = models.ForeignKey(Listing, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
---
+++ commerce/auctions/views.py
@@ -0,0 +1,109 @@
+from django.contrib.auth import authenticate, login, logout
+from django.db import IntegrityError
+from django.http import HttpResponse, HttpResponseRedirect
+from django.shortcuts import render
+from django.urls import reverse
+
+from .models import *
+
+
+def index(request):
+    listings = Listing.objects.all()
+
+    return render(request, "auctions/index.html",{
+                  "listings": listings
+                  })
+
+def listing(request, id):
+    listings = Listing.objects.filter(id=id)
+
+    return render(request, "auctions/index.html",{
+                  "listings": listings
+                  })
+
+def login_view(request):
+    if request.method == "POST":
+
+        # Attempt to sign user in
+        username = request.POST["username"]
+        password = request.POST["password"]
+        user = authenticate(request, username=username, password=password)
+
+        # Check if authentication successful
+        if user is not None:
+            login(request, user)
+            return HttpResponseRedirect(reverse("index"))
+        else:
+            return render(request, "auctions/login.html", {
+                "message": "Invalid username and/or password."
+            })
+    else:
+        return render(request, "auctions/login.html")
+
+
+def logout_view(request):
+    logout(request)
+    return HttpResponseRedirect(reverse("index"))
+
+
+def register(request):
+    if request.method == "POST":
+        username = request.POST["username"]
+        email = request.POST["email"]
+
+        # Ensure password matches confirmation
+        password = request.POST["password"]
+        confirmation = request.POST["confirmation"]
+        if password != confirmation:
+            return render(request, "auctions/register.html", {
+                "message": "Passwords must match."
+            })
+
+        # Attempt to create new user
+        try:
+            user = User.objects.create_user(username, email, password)
+            user.save()
+        except IntegrityError:
+            return render(request, "auctions/register.html", {
+                "message": "Username already taken."
+            })
+        login(request, user)
+        return HttpResponseRedirect(reverse("index"))
+    else:
+        return render(request, "auctions/register.html")
+
+
+def create_listing(request):
+    if request.method == "POST":
+        title = request.POST["title"]
+        description = request.POST["description"]
+        #testing get since POST is a dictionary by default
+        start_bid = request.POST.get("starting_bid", "")
+        image_url = request.POST["image_url"]
+        #error check
+        errors = {}
+
+        if title == "":
+            errors["title"]="title cannot be blank"
+        
+        if start_bid == "":
+            start_bid = 0.01
+        else:
+            try:
+                start_bid = float(start_bid)
+            except ValueError:
+                errors["starting_bid"]="an issue with the starting bid format"
+        
+        if errors:
+            return render (request, "auctions/create_listing.html", {
+                    "errors": errors
+                })
+    
+        #pass user.id from request
+        listing = Listing(user=request.user, title=title, description=description, start_bid=start_bid, current_price=start_bid, image_url=image_url)
+        listing.save()
+        return HttpResponseRedirect(reverse("index"))
+    
+    else:
+        #return default page if not post form
+        return render(request, "auctions/create_listing.html")
